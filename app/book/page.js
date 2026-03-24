1355d4327b756a8b8d7f0118c33d99bdbc62cf46
Casey <casey@huddly.com>
2026-03-24 17:57:29 -0500

    add stub pages

---
+++ app/book/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ textAlign: "center", padding: "4rem 2rem" }}>
+      <h1>Coming Soon</h1>
+      <p>This page is under construction.</p>
+    </main>
+  );
+}
